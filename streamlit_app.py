ded0dbee24c74acd1bf0b5db9c379b384576f8ba
Bethany Marshall <82471699+BethDataHub@users.noreply.github.com>
2022-04-23 19:22:14 +0100

    Update streamlit_app.py
    amend insert row to be in a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ from urllib.error import URLError
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -54,10 +55,13 @@ if streamlit.button('Get Fruit Load List'):
 
 
 ##Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("INSERT INTO FRUIT_LOAD_LIST (FRUIT_NAME) VALUES ('from streamlit')")
+        return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-
-if add_my_fruit != '':
-    my_cur.execute("INSERT INTO FRUIT_LOAD_LIST (FRUIT_NAME) VALUES ('"+add_my_fruit+"')")
-    streamlit.write('Thanks for adding', add_my_fruit)
-
+if streamlit.button('Add a fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
