00326bb1c0269de1a905c98376c2847ad014f77a
Bethany Marshall <82471699+BethDataHub@users.noreply.github.com>
2022-04-23 19:25:46 +0100

    Update streamlit_app.py
    Added comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from urllib.error import URLError
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
+##Hard-coded header and menu
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -17,20 +17,23 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+##Section allowing end upser to build their own smoothie
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# Let's put a pick list here so they can pick the fruit they want to include
+##Show the fruits that have been chosen
 fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page
+##Display the fruit list table on the page
 streamlit.dataframe(fruits_to_show)
 
+##Get the fruit information from the fruityvice api
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
+##Allow user to search for fruit in fruityvice api
 streamlit.header('Fruityvice Fruit Advice!')
 try:    
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -42,6 +45,7 @@ try:
 except URLError as e:
     streamlit.error()
 
+##Show the fruit load list
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
